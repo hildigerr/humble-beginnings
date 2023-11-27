0c00d11cecb1236e79b212d680a3bd3ada46cd9e
Hildigerr Vergaray <Hildigerr.Vergaray@YmirSystems.com>
2023-11-27 15:23:06 -0800

    update c2f to recieve temp parameter

--- c2f.c
+++ c2f.c
@@ -1,14 +1,24 @@
-/* f=9/5*c+32 */
+/* f=9/5*c+32 *///Add -r for "reverse" to c2f/f2c
 #include <stdio.h>
+#include <stdlib.h>
 
-int main(void)
+int main( int argc, char* argv[] )
 {
-    float f=32, c;
+    double f = 32.0, c;
 
-    printf("\nWhat is the temperature you whish to convert, in celcious? ");
-    scanf("%f", &c);
-    f += (1.8*c);
-    printf("%.2f\n", f);
+    if( argv[0][0] == 'c' ) {
+        if( argc < 2 ) {
+            printf("\nWhat is the temperature you whish to convert, in celcious? ");
+            scanf("%lf", &c);
+        } else c = atof( argv[1] );
+        f += ( 1.8 * c );
+    } else { /* f2c */
+        if( argc < 2 ) {
+            printf("\nWhat is the temperature you whish to convert, in fahrenheit? ");
+            scanf("%lf", &c);
+        } else c = atof( argv[1] );
+        f = ((c - f)*(5/9));
+    } printf("%.2f\n", f);
 
     return 0;
-}
+}/* End main Func */
